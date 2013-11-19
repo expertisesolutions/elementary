3f2cfb635b5ccbe73c2074eb12e93caa3e4daf20
Felipe Magno de Almeida <eu@felipemagno.com.br>
2013-11-19 18:41:40 -0200

    Made example compile with the necessary declarations

--- src/examples/filemodel_example.c
+++ src/examples/filemodel_example.c
@@ -1,15 +1,40 @@
 //Compile (not yet) with:
 //gcc -g filemodel_example.c -o filemodel_example `pkg-config --cflags --libs elementary`
 
+#define EFL_EO_API_SUPPORT
+#define EFL_BETA_API_SUPPORT
+
+#include <Eo.h>
 #include <Elementary.h>
+
+typedef Eo Elm_Tree_View_Class;
+typedef Eo Elm_List_View_Class;
 
 static Elm_Tree_View_Class *_tree_v = NULL;
 static Elm_List_View_Class *_list_v = NULL;
 
+typedef Eo Eo_File_Model_Class;
+typedef Eo Eo_List_Node_Model_Class;
+
 static Eo_File_Model_Class *_file_m = NULL; //implements file as a tree_data_model
 static Eo_List_Node_Model_Class *_list_m = NULL; //show the selected group_node content as a list
 
-static void
+Eo_File_Model_Class* eo_file_model_class_new(const char* path);
+Eo_List_Node_Model_Class* eo_list_node_model_class_new(Eo_File_Model_Class* file_model);
+
+Elm_Tree_View_Class*
+elm_tree_view_add(Evas_Object* parent, Eo_File_Model_Class* file_model);
+
+Elm_Tree_View_Class* elm_tree_view_add(Evas_Object* parent, Eo_File_Model_Class* file_model);
+Elm_List_View_Class* elm_list_view_add(Evas_Object* parent, Eo_List_Node_Model_Class* list_model);
+
+#define elm_tree_view_mode_set(mode) 0, EO_TYPECHECK(int, 5)
+#define elm_tree_view_evas_object_get(evas) 0, EO_TYPECHECK(Evas_Object**, evas)
+
+extern const Eo_Event_Description _EO_EV_LISTMODEL_ITEM_SELECTED;
+#define EO_EV_LISTMODEL_ITEM_SELECTED (&(_EO_EV_LISTMODEL_ITEM_SELECTED))
+
+static Eina_Bool
 _item_sel_cb(void *data, Eo *obj, const Eo_Event_Description *desc, void *event_info)
 {
    Evas_Object *label = (Evas_Object *) data;
@@ -47,8 +72,8 @@ elm_main(int argc, char **argv)
    evas_object_show(box);
 
    //Directories tree widget
-   eo_do(_tree_v, elm_tree_view_mode_set, ELM_TREE_VIEW_VIEWMODE_GROUP); //hide files, show only directories
-   eo_do(_tree_v, elm_tree_view_evas_object_get, &widget); //or tree_view inherits genlist
+   eo_do(_tree_v, elm_tree_view_mode_set(ELM_TREE_VIEW_VIEWMODE_GROUP)); //hide files, show only directories
+   eo_do(_tree_v, elm_tree_view_evas_object_get(&widget)); //or tree_view inherits genlist
 
    evas_object_size_hint_weight_set(widget, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(widget, EVAS_HINT_FILL, EVAS_HINT_FILL);
@@ -56,9 +81,8 @@ elm_main(int argc, char **argv)
    elm_box_pack_end(box, widget);
    evas_object_show(widget);
 
-
    //file list widget
-   eo_do(_list_v, elm_list_view_evas_object_get, &widget); //or tree_view inherits genlist
+   eo_do(_list_v, elm_list_view_evas_object_get(&widget)); //or tree_view inherits genlist
 
    evas_object_size_hint_weight_set(widget, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(widget, EVAS_HINT_FILL, EVAS_HINT_FILL);
@@ -74,8 +98,7 @@ elm_main(int argc, char **argv)
    evas_object_show(label);
  
    //add callback
-   eo_do(_lis_m, eo_event_callback_add(EO_EV_LISTMODEL_ITEM_SELECTED, _item_sel_cb, label));
-
+   eo_do(_list_m, eo_event_callback_add(EO_EV_LISTMODEL_ITEM_SELECTED, _item_sel_cb, (void*)label));
 
    evas_object_resize(win, 800, 400);
    evas_object_show(win);
