01af7e70d3652b3440176a9e46927a78a623f3ca
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-02-20 19:05:06 -0300

    mvc: list const and list mutable (or simply list) is now EO.

--- src/lib/elm_model_list.c
+++ src/lib/elm_model_list.c
@@ -9,17 +9,24 @@
 
 #include <assert.h>
 
-/// Const interface ///
+// For const interface
+#define MY_MODEL_LIST_CONST_CLASS ELM_OBJ_MODEL_LIST_CONST_CLASS
+EAPI Eo_Op ELM_OBJ_MODEL_LIST_CONST_BASE_ID = 0;
 
+// For mutable interface
+
+/// Const interface ///
 static void
 _model_list_item_select(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list EINA_UNUSED)
 {
+   //TODO: implement
    return;
 }
 
 static void
 _model_list_selected_get(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
 {
+   //TODO: implement
    Elm_Model_List_Index *index = va_arg(*list, Elm_Model_List_Index *);
    EINA_SAFETY_ON_NULL_RETURN(index);
    *index = (Elm_Model_List_Index)-1; 
@@ -28,6 +35,7 @@ _model_list_selected_get(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_l
 static void
 _model_list_len(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
 {
+   //TODO: implement
    long *len = va_arg(*list, long *);
    EINA_SAFETY_ON_NULL_RETURN(len);
    *len = 0L;
@@ -36,44 +44,162 @@ _model_list_len(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list
 static void
 _model_list_value_get(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
 {
+   //TODO: implement
    Eina_Value **value = va_arg(*list, Eina_Value **);
    EINA_SAFETY_ON_NULL_RETURN(*value);
    *value = EINA_FALSE;
 }
-
-/// Mutable interface ///
-
-static Elm_Model_List_Index
-_model_list_item_append(Eina_Value *value)
+      
+static void
+_model_list_constructor(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list EINA_UNUSED)
 {
-   return (Elm_Model_List_Index)-1;
-}
-
-static Elm_Model_List_Index
-_model_list_item_prepend(Eina_Value *value)
-{
-   return (Elm_Model_List_Index)-1;
-}
-
-static Elm_Model_List_Index
-_model_list_item_append_relative(Elm_Model_List_Index index, Eina_Value *value)
-{
-   return (Elm_Model_List_Index)-1;
-}
-
-static Elm_Model_List_Index
-_model_list_item_prepend_relative(Elm_Model_List_Index index, Eina_Value *value)
-{
-   return (Elm_Model_List_Index)-1;
+   //TODO: implement
+   eo_do_super(obj, MY_MODEL_LIST_CONST_CLASS, eo_constructor());
 }
 
 static void
-_model_list_item_delete(Elm_Model_List_Index index)
+_model_list_destructor(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list EINA_UNUSED)
 {
+   //TODO: implement
+   eo_do_super(obj, MY_MODEL_LIST_CONST_CLASS, eo_destructor());
 }
 
-/// Class definition ///
+static void
+_model_list_const_class_constructor(Eo_Class *klass)
+{
+   const Eo_Op_Func_Description func_descs[] = {
+      EO_OP_FUNC(EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), _model_list_constructor),
+      EO_OP_FUNC(EO_BASE_ID(EO_BASE_SUB_ID_DESTRUCTOR), _model_list_destructor),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_CONST_ID(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_ITEM_SELECT), _model_list_item_select),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_CONST_ID(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_VALUE_GET), _model_list_value_get),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_CONST_ID(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_SELECTED_GET), _model_list_selected_get),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_CONST_ID(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_LIST_LEN), _model_list_len),
+      EO_OP_FUNC_SENTINEL
+   };
 
-/* EO3_DEFINE_CLASS(ELM_MODEL_LIST_CONST_CLASS, ((EO3_BASE_CLASS)), NULL) */
+   eo_class_funcs_set(klass, func_descs);
+}
 
-/* EO3_DEFINE_CLASS(ELM_MODEL_LIST_CLASS, ((ELM_MODEL_LIST_CONST_CLASS)), NULL) */
+static const Eo_Op_Description model_list_const_op_descs[] = {
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_ITEM_SELECT, "Select the node pointed by path."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_VALUE_GET, "Get the value of the node pointed by path."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_SELECTED_GET, "Get the children of the node pointed by path."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_CONST_SUB_ID_LIST_LEN, "Count number of children of the node pointed by path."),
+   EO_OP_DESCRIPTION_SENTINEL
+};
+
+static Eo_Class_Description model_list_const_class_descs = {
+   EO_VERSION,
+   "Model List Const",
+   EO_CLASS_TYPE_REGULAR,
+   EO_CLASS_DESCRIPTION_OPS(&ELM_OBJ_MODEL_LIST_CONST_BASE_ID, model_list_const_op_descs, ELM_OBJ_MODEL_LIST_CONST_SUB_ID_LAST),
+   NULL, // event_descs
+   0, //private data sizeof
+   _model_list_const_class_constructor,
+   NULL
+};
+
+EO_DEFINE_CLASS(elm_obj_model_list_const_class_get, &model_list_const_class_descs, EO_BASE_CLASS, NULL);
+
+/// Mutable interface ///
+
+static void 
+_model_list_item_append(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Eina_Value *value = va_arg(*list, Eina_Value *);
+   Elm_Model_List_Index *ret = va_arg(*list, Elm_Model_List_Index *);
+
+   *ret = (Elm_Model_List_Index)-1;
+}
+
+static void 
+_model_list_item_prepend(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Eina_Value *value = va_arg(*list, Eina_Value *);
+   Elm_Model_List_Index *ret = va_arg(*list, Elm_Model_List_Index *);
+
+   *ret = (Elm_Model_List_Index)-1;
+}
+
+static void 
+_model_list_item_append_relative(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Elm_Model_List_Index index = va_arg(*list, Elm_Model_List_Index);
+   Eina_Value *value = va_arg(*list, Eina_Value *);
+   Elm_Model_List_Index *ret = va_arg(*list, Elm_Model_List_Index *);
+
+   *ret = (Elm_Model_List_Index)-1;
+}
+
+static void 
+_model_list_item_prepend_relative(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Elm_Model_List_Index index = va_arg(*list, Elm_Model_List_Index);
+   Eina_Value *value = va_arg(*list, Eina_Value *);
+   Elm_Model_List_Index *ret = va_arg(*list, Elm_Model_List_Index *);
+
+   *ret = (Elm_Model_List_Index)-1;
+}
+
+static void 
+_model_list_item_delete(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Elm_Model_List_Index index = va_arg(*list, Elm_Model_List_Index);
+}
+
+static void
+_model_list_value_set(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, va_list *list)
+{
+   //TODO: implement
+   Elm_Model_List_Index index = va_arg(*list, Elm_Model_List_Index);
+   Eina_Value *value = va_arg(*list, Eina_Value *);
+}
+
+
+#define MY_MODEL_LIST_CLASS ELM_OBJ_MODEL_LIST_CONST_CLASS
+EAPI Eo_Op ELM_OBJ_MODEL_LIST_BASE_ID = 0;
+
+static void
+_model_list_class_constructor(Eo_Class *klass)
+{
+   const Eo_Op_Func_Description func_descs[] = {
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND), _model_list_item_append),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND), _model_list_item_prepend),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND_RELATIVE), _model_list_item_append_relative),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND_RELATIVE), _model_list_item_prepend_relative),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_DELETE), _model_list_item_delete),
+      EO_OP_FUNC(ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_VALUE_SET), _model_list_value_set),
+      EO_OP_FUNC_SENTINEL
+   };
+
+   eo_class_funcs_set(klass, func_descs);
+}
+
+static const Eo_Op_Description model_list_op_descs[] = {
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND, "Append new item."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND, "Prepend new item."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND_RELATIVE, "Append as a sibling node."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND_RELATIVE, "Prepend as a sibling node."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_DELETE, "Delete item."),
+   EO_OP_DESCRIPTION(ELM_OBJ_MODEL_LIST_SUB_ID_VALUE_SET, "Set value to the node."),
+   EO_OP_DESCRIPTION_SENTINEL
+};
+
+static Eo_Class_Description model_list_class_descs = {
+   EO_VERSION,
+   "Model List",
+   EO_CLASS_TYPE_REGULAR,
+   EO_CLASS_DESCRIPTION_OPS(&ELM_OBJ_MODEL_LIST_BASE_ID, model_list_op_descs, ELM_OBJ_MODEL_LIST_SUB_ID_LAST),
+   NULL, // event_descs
+   0, //private data sizeof
+   _model_list_class_constructor,
+   NULL
+};
+
+EO_DEFINE_CLASS(elm_obj_model_list_class_get, &model_list_class_descs, ELM_OBJ_MODEL_LIST_CONST_CLASS, NULL);
+
--- src/lib/elm_model_list_mutable.h
+++ src/lib/elm_model_list_mutable.h
@@ -2,6 +2,21 @@
 #ifndef ELM_MODEL_LIST_MUTABLE_H
 #define ELM_MODEL_LIST_MUTABLE_H
 
+extern EAPI Eo_Op ELM_OBJ_MODEL_LIST_BASE_ID;
+
+
+enum {
+   ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND,
+   ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND,
+   ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND_RELATIVE,
+   ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND_RELATIVE,
+   ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_DELETE,
+   ELM_OBJ_MODEL_LIST_SUB_ID_VALUE_SET,
+   ELM_OBJ_MODEL_LIST_SUB_ID_LAST
+};
+
+#define ELM_OBJ_MODEL_LIST_ID(sub_id) (ELM_OBJ_MODEL_LIST_BASE_ID + sub_id)
+
 /**
  * @def elm_model_list_item_append
  * @since 1.8
@@ -11,7 +26,7 @@
  *
  * Append @p value to this list model.
  */
-EAPI Elm_Model_List_Index elm_model_list_item_append(Eina_Value* value);
+#define elm_model_list_item_append(value, ret) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND), EO_TYPECHECK(Eina_Value *, value), EO_TYPECHECK(Elm_Model_List_Index *, ret)
 
 
 /**
@@ -23,8 +38,7 @@ EAPI Elm_Model_List_Index elm_model_list_item_append(Eina_Value* value);
  *
  * Prepend @p value to this list model.
  */
-EAPI Elm_Model_List_Index elm_model_list_item_prepend(Eina_Value* value);
-
+#define elm_model_list_item_prepend(value, ret) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND), EO_TYPECHECK(Eina_Value *, value), EO_TYPECHECK(Elm_Model_List_Index *, ret)
 
 /**
  * @def elm_model_list_item_append_relative
@@ -36,8 +50,7 @@ EAPI Elm_Model_List_Index elm_model_list_item_prepend(Eina_Value* value);
  *
  * Append @p value to this list model after position @p index.
  */
-EAPI Elm_Model_List_Index elm_model_list_item_append_relative(Elm_Model_List_Index index,
-                                                              Eina_Value* value);
+#define elm_model_list_item_append_relative(index, value ,ret) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_APPEND_RELATIVE), EO_TYPECHECK(Elm_Model_List_Index, index), EO_TYPECHECK(Eina_Value *, value), EO_TYPECHECK(Elm_Model_List_Index *, ret)
 
 /**
  * @def elm_model_list_item_prepend_relative
@@ -49,8 +62,7 @@ EAPI Elm_Model_List_Index elm_model_list_item_append_relative(Elm_Model_List_Ind
  *
  * Prepend @p value to this list model before position @p index.
  */
-EAPI Elm_Model_List_Index elm_model_list_item_prepend_relative(Elm_Model_List_Index index,
-                                                               Eina_Value* value);
+#define elm_model_list_item_prepend_relative(index, value ,ret) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_PREPEND_RELATIVE), EO_TYPECHECK(Elm_Model_List_Index, index), EO_TYPECHECK(Eina_Value *, value), EO_TYPECHECK(Elm_Model_List_Index *, ret)
 
 /**
  * @def elm_model_list_item_delete
@@ -60,7 +72,7 @@ EAPI Elm_Model_List_Index elm_model_list_item_prepend_relative(Elm_Model_List_In
  *
  * Delete the item at position @p index.
  */
-EAPI void elm_model_list_item_delete(Elm_Model_List_Index index);
+#define elm_model_list_item_delete(index) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_ITEM_DELETE), EO_TYPECHECK(Elm_Model_List_Index, index)
 
 /**
  * @def elm_model_list_value_set
@@ -71,21 +83,10 @@ EAPI void elm_model_list_item_delete(Elm_Model_List_Index index);
  *
  * Set a value to an item of this list model.
  */
-EAPI void elm_model_list_value_set(Elm_Model_List_Index index, Eina_Value* value);
+#define elm_model_list_value_set(index, value) ELM_OBJ_MODEL_LIST_ID(ELM_OBJ_MODEL_LIST_SUB_ID_VALUE_SET), EO_TYPECHECK(Elm_Model_List_Index, index), EO_TYPECHECK(Eina_Value *, value)
 
-/* /\** */
-/*  * @brief EO3 Interface Declaration */
-/*  *\/ */
-/* #define ELM_MODEL_LIST_CLASS elm_model_list                                                           \ */
-/*  , function(elm_model_list_item_append, _model_list_item_append, Elm_Model_List_Index, Eina_Value*)   \ */
-/*  , function(elm_model_list_item_prepend, _model_list_item_prepend, Elm_Model_List_Index, Eina_Value*) \ */
-/*  , function(elm_model_list_item_append_relative, _model_list_item_append_relative,                    \ */
-/*             Elm_Model_List_Index, Elm_Model_List_Index, Eina_Value*)                                  \ */
-/*  , function(elm_model_list_item_prepend_relative, _model_list_item_prepend_relative,                  \ */
-/*             Elm_Model_List_Index, Elm_Model_List_Index, Eina_Value*)                                  \ */
-/*  , function(elm_model_list_item_delete, _model_list_item_delete, void, Elm_Model_List_Index) */
-/* , function(elm_model_list_value_set, void, Elm_Model_List_Index, Eina_Value*) */
 
-/* EO3_DECLARE_CLASS(ELM_MODEL_LIST_CLASS) */
+#define ELM_OBJ_MODEL_LIST_CLASS elm_obj_model_list_class_get()
+const Eo_Class *elm_obj_model_list_class_get(void);
 
 #endif // ELM_MODEL_LIST_MUTABLE_H
