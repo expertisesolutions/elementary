570a23e2f0ea8c7889996c52f9f235ceb94dfec9
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-04-24 13:11:32 -0300

    view_form: renamed function

--- src/lib/elm_view_form.h
+++ src/lib/elm_view_form.h
@@ -11,13 +11,13 @@ enum {
 
 #define ELM_VIEW_FORM_ID(sub_id) (ELM_VIEW_FORM_BASE_ID + sub_id)
 /**
- * @def elm_view_form_add
+ * @def elm_view_form_setup
  * @since 1.10
  *
  * @param name
  * @param title
  *
  */
-#define elm_view_form_add(name, title) EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), EO_TYPECHECK(const char *, name), EO_TYPECHECK(const char *, title)
+#define elm_view_form_setup(name, title) EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), EO_TYPECHECK(const char *, name), EO_TYPECHECK(const char *, title)
 
 #endif
