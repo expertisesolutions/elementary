ef2554f1d426f5c793036c0ccf348560718f1ef5
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-04-24 13:10:23 -0300

    view_form: comment.

--- src/lib/elm_view_form.h
+++ src/lib/elm_view_form.h
@@ -11,11 +11,11 @@ enum {
 
 #define ELM_VIEW_FORM_ID(sub_id) (ELM_VIEW_FORM_BASE_ID + sub_id)
 /**
- * @def elm_view_list_add
- * @since 1.8
+ * @def elm_view_form_add
+ * @since 1.10
  *
- * @param parent
- * @param model
+ * @param name
+ * @param title
  *
  */
 #define elm_view_form_add(name, title) EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), EO_TYPECHECK(const char *, name), EO_TYPECHECK(const char *, title)
